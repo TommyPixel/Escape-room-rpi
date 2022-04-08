e5d7b1d482c1bb0d1d161a59c5e885db9e8937d1
TommyPixel <tommy@pixelstudio.be>
2022-04-08 17:07:35 +0200

    Styling added (Not working) RPI GPIO done

--- rpinodeled/nodeled.js
+++ rpinodeled/nodeled.js
@@ -3,34 +3,47 @@ var app = express();
 var path = require('path');
 var gpio = require('rpi-gpio');
 gpio.setup(12, gpio.DIR_OUT);
+gpio.setup(11, gpio.DIR_OUT);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 console.log(path.join(__dirname, 'public'));
 app.get('/', function(req, res){ 
-            res.render('index',{status:"Press Button To change Status of Led !!"});
+            res.render('index',{status:"answer the Question"});
 });
 app.post('/led/correct', function(req, res){
-gpio.write(32, false, function(err) {
-        if (err) throw err;
-            return;
-    });
 gpio.write(12, true, function(err) {
         if (err) throw err;
-        console.log('Written True to pin');
+        console.log('Written True to pin 12');
             console.log(path.join(__dirname, 'public'));
             return res.render('index', {status: "Answer Was Correct"});
     });
 });
 app.post('/led/incorrect', function(req, res){
-gpio.write(32, true, function(err) {
-        if (err) throw err;
-            return;
-    });
 gpio.write(12, false, function(err) {
         if (err) throw err;
-        console.log('Written False to pin');
+        console.log('Written False to pin 12');
             console.log(path.join(__dirname, 'public'));
             return res.render('index',{status: "Answer Was Wrong"});
+    });
+gpio.write(11, true, function(err) {
+        if (err) throw err;
+        console.log('Written true to pin 10');
+            console.log(path.join(__dirname, 'public'));
+            return;
+    });
+});
+app.post('/led/off', function(req, res){
+gpio.write(12, false, function(err) {
+        if (err) throw err;
+        console.log('Written False to pin 12');
+            console.log(path.join(__dirname, 'public'));
+            return res.render('index',{status: "Answer Was Wrong"});
+    });
+gpio.write(11, false, function(err) {
+        if (err) throw err;
+        console.log('Written false to pin 10');
+            console.log(path.join(__dirname, 'public'));
+            return;
     });
 });
 app.listen(4000, function () {
