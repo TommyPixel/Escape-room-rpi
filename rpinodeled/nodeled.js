75b0a711984a0f9fda48d6cdfb4b3cc065c9416a
Tommypixel <tommy@pixelstudio.be>
2022-04-13 14:11:30 +0200

    added unlocks

--- rpinodeled/nodeled.js
+++ rpinodeled/nodeled.js
@@ -1,16 +1,19 @@
     var express = require('express'); 
 var app = express();
 var path = require('path');
-//var gpio = require('rpi-gpio');
-//gpio.setup(12, gpio.DIR_OUT);
-//gpio.setup(11, gpio.DIR_OUT);
+var gpio = require('rpi-gpio');
+gpio.setup(12, gpio.DIR_OUT);
+gpio.setup(16, gpio.DIR_OUT);
+gpio.setup(11, gpio.DIR_OUT);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 console.log(path.join(__dirname, 'public'));
 app.get('/', function(req, res){ 
             res.render('index',{status:"Waiting for your answer..."});
 });
-app.post('/led/correct', function(req, res){
+
+//Correct Answer
+app.get('/correct', function(req, res){
 gpio.write(12, true, function(err) {
         if (err) throw err;
         console.log('Written True to pin 12');
@@ -18,7 +21,31 @@ gpio.write(12, true, function(err) {
             return res.render('index', {status: "Answer Was Correct"});
     });
 });
-app.post('/led/incorrect', function(req, res){
+
+//Unlock Door
+app.get('/unlock', function(req, res){
+gpio.write(16, true, function(err) {
+        if (err) throw err;
+        console.log('Written True to pin 16');
+            console.log(path.join(__dirname, 'public'));
+            return res.render('index', {status: "Answer Was Correct"});
+    });
+gpio.write(12, false, function(err) {
+        if (err) throw err;
+        console.log('Written False to pin 12');
+            console.log(path.join(__dirname, 'public'));
+            return res.render('index',{status: "Answer Was Wrong"});
+    });
+gpio.write(11, false, function(err) {
+        if (err) throw err;
+        console.log('Written false to pin 10');
+            console.log(path.join(__dirname, 'public'));
+            return;
+    });
+});
+
+//Incorrect answer
+app.get('/Incorrect', function(req, res){
 gpio.write(12, false, function(err) {
         if (err) throw err;
         console.log('Written False to pin 12');
@@ -32,7 +59,9 @@ gpio.write(11, true, function(err) {
             return;
     });
 });
-app.post('/led/off', function(req, res){
+
+//restart
+app.get('/Off', function(req, res){
 gpio.write(12, false, function(err) {
         if (err) throw err;
         console.log('Written False to pin 12');
@@ -42,6 +71,13 @@ gpio.write(12, false, function(err) {
 gpio.write(11, false, function(err) {
         if (err) throw err;
         console.log('Written false to pin 10');
+            console.log(path.join(__dirname, 'public'));
+            return;
+    });
+
+gpio.write(16, false, function(err) {
+        if (err) throw err;
+        console.log('Written false to pin 16');
             console.log(path.join(__dirname, 'public'));
             return;
     });
